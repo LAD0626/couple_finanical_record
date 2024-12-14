304ab978de6c6fd4d28950e470626c859361012a
dididadida <andie@dididadidadebijixingdiannao.local>
2024-12-14 21:19:16 +0800

    Initial commit

---
+++ Financial_recoder/Financial_recoder/Financial_recoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Financial_recoderApp.swift
+//  Financial_recoder
+//
+//  Created by dididadida on 2024/12/14.
+//
+
+import SwiftUI
+
+@main
+struct Financial_recoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
